5a0a580603284df8a2e9d37eac7e0ff8a0c50c0f
Ariel Armijo <arielarmijo@yahoo.es>
2023-05-14 10:43:16 -0400

    Play song when switch to number layer

--- keyboards/planck/keymaps/aarmijo/keymap.c
+++ keyboards/planck/keymaps/aarmijo/keymap.c
@@ -17,10 +17,6 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
-enum tap_dances {
-  TD_UP_APP,
-};
-
 enum planck_layers {
   _QWERTY,
   _COLEMAK,
@@ -28,7 +24,7 @@ enum planck_layers {
   _PLOVER,
   _LOWER,
   _RAISE,
-  _NUMBER,
+  _NUMBERS,
   _ADJUST,
   _FUNCTION
 };
@@ -39,13 +35,20 @@ enum planck_keycodes {
   DVORAK,
   PLOVER,
   EXT_PLV,
+  NUMBERS,
   RUT,
   EMAIL,
   PHONE,
-	CC
+	CC,
+  USR,
+  PWD,
+  BD
 };
 
-#define DEFAULT TO(_QWERTY)
+enum tap_dances {
+  TD_UP_APP,
+};
+
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define NUMBER TG(_NUMBER)
@@ -130,7 +133,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,    KC_8,       KC_9,     KC_0,    KC_DEL,
     KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_LEFT,    KC_DOWN, KC_UP,      KC_RIGHT, KC_TRNS, KC_GRV,
     KC_TRNS, KC_MPRV, KC_VOLD, KC_MPLY, KC_VOLU, KC_MNXT, RALT(KC_N), KC_MUTE, S(KC_NUBS), KC_TRNS,  KC_HOME, KC_END,
-    KC_TRNS, NUMBER,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_MPRV,    KC_VOLD,  KC_VOLU, KC_MPLY
+    KC_TRNS, NUMBERS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_MPRV,    KC_VOLD,  KC_VOLU, KC_MPLY
   ), 
 
   /* Raise */
@@ -142,11 +145,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   /* Numbers */
-  [_NUMBER] = LAYOUT_planck_grid(
-    DEFAULT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_P7, KC_P8,   KC_P9,   KC_0,    KC_DEL,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P4, KC_P5,   KC_P6,   KC_PAST, KC_PSLS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1, KC_P2,   KC_P3,   KC_PMNS, KC_PEQL,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PPLS, KC_PPLS, KC_P0, KC_PDOT, KC_TRNS, KC_TRNS, KC_TRNS
+  [_NUMBERS] = LAYOUT_planck_grid(
+    QWERTY,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_P7, KC_P8,   KC_P9,   KC_0,    KC_BSPC,
+    KC_DEL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P4, KC_P5,   KC_P6,   KC_PAST, KC_PSLS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1, KC_P2,   KC_P3,   KC_PMNS, KC_PENT,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PPLS, KC_PPLS, KC_P0, KC_PDOT, KC_TRNS, KC_TRNS, KC_NUM
   ),
 
   /* Adjust (Lower + Raise) */
@@ -160,8 +163,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Function */
   [_FUNCTION] = LAYOUT_planck_grid(
     KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
-    KC_TRNS, RUT,     EMAIL,   PHONE,   CC,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR,
+    KC_TRNS, RUT,     EMAIL,   PHONE,   CC,      BD,      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, USR,     PWD,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BRID, KC_BRIU, KC_TRNS
   )
 
@@ -170,6 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef AUDIO_ENABLE
   float plover_song[][2] = SONG(PLOVER_SOUND);
   float plover_gb_song[][2] = SONG(PLOVER_GOODBYE_SOUND);
+  float planck_song[][2] = SONG(PLANCK_SOUND);
 #endif
 
 layer_state_t layer_state_set_user(layer_state_t state) {
@@ -224,6 +228,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case NUMBERS:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_NUMBERS);
+        PLAY_SONG(planck_song);
+      }
+      return false;
+      break;
     case RUT:
       if (record->event.pressed) {
         SEND_STRING("13658100-7");
@@ -242,6 +253,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		case CC:
       if (record->event.pressed) {
         SEND_STRING("4345591123200648");
+      }
+      break;
+    case BD:
+      if (record->event.pressed) {
+        SEND_STRING("Good morning team!");
+      }
+      break;
+    case USR:
+      if (record->event.pressed) {
+        SEND_STRING("aarmijo");
+      }
+      break;
+    case PWD:
+      if (record->event.pressed) {
+        SEND_STRING("cg331x");
       }
       break;
   }
